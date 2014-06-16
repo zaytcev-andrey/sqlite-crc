93596d629245bc711430dd466f48b2fd8048d4c9
andrey-zaytcev <andrey-zaytcev@narod.ru>
2014-06-16 23:22:44 +0400

    refactoring: read page functions moved to db_page_reader.c

--- codecext.c
+++ codecext.c
@@ -1,14 +1,10 @@
 #include "checked_codec.c"
-#include "md5.h"
-#include "freelist_set.h"
-#include "utils.h"
+#include "db_page_reader.c"
 
 #include <stdio.h>
 
 #ifndef SQLITE_OMIT_DISKIO
 #ifdef SQLITE_HAS_CODEC
-
-#define HASH_SIZE 16
 
 // Guessing that "see" is related to SQLite Encryption Extension" (the semi-official, for-pay, encryption codec)
 // Just as useful for initializing.
@@ -28,6 +24,52 @@ void sqlite3PagerFreeCodec(void *pCodec)
 // Report the page size to the codec, called from pager.c (address passed in sqlite3PagerSetCodec)
 void sqlite3CodecSizeChange(void *pCodec, int pageSize, int nReserve)
 {
+}
+
+static int GetDbOpenState( Btree* btree, db_info* db_i );
+static int SetDbReservedState( sqlite3* db, Btree* btree, check_crc* crc_impl );
+
+int GetDbOpenState( Btree* btree, db_info* db_i )
+{
+     sqlite3_file* f_db = sqlite3BtreePager( btree )->fd;
+
+     int rc = SQLITE_ERROR;
+     db_i->open_state = DB_CLOSED;
+
+     if( !isOpen( f_db ) )
+     {
+          rc = sqlite3OsFileSize( f_db, &db_i->file_size );
+          if( rc == SQLITE_IOERR_SHORT_READ )
+          {
+               rc = SQLITE_OK;
+          }
+
+          if ( rc == SQLITE_OK )
+          {
+               db_i->open_state = db_i->file_size ? 
+                    DB_OPENED_EXISTING : DB_OPENED_CREATING;
+          }
+     }
+
+     return rc;
+}
+
+int SetDbReservedState( sqlite3* db, Btree* btree, check_crc* crc_impl )
+{
+     int rc = SQLITE_ERROR;
+     int reserve = sqlite3BtreeGetReserve( btree );
+     FILE* f_db = 0;
+
+     if ( reserve != crc_impl->xGetCrcLength() && reserve >= 0 )
+     {
+          reserve = crc_impl->xGetCrcLength();
+          rc = sqlite3BtreeSetPageSize( btree, -1, reserve, 0 );
+
+          if ( rc != SQLITE_OK )
+          {
+               sqlite3Error(db, rc, "unable to reserve page space for the codec");
+          }
+     }
 }
 
 void* GetPageCopyWithReservedSpace( void* data, int page_size, checked_codec* ch_codec )
@@ -95,194 +137,6 @@ void* sqlite3Codec(void *pCodec, void *data, Pgno nPageNum, int nMode)
 
   return data;
 
-}
-
-typedef enum { DB_OPENED_CREATING, DB_OPENED_EXISTING, DB_CLOSED } db_open_state;
-
-typedef struct DB_INFO 
-{
-     db_open_state open_state;
-     int current_page_number;
-     int freelist_trunk_page_number;
-     int freelist_trunk_page_offset;
-     int total_number_of_freelist_pages; // count of all ( trunk and leaf ) pages in freelist 
-     i64 current_page_offset;
-     int page_header_offset;
-     int page_size;
-     i64 file_size;
-     unsigned char* page_buff;
-} db_info;
-
-static int ReadDbHeader( sqlite3_file* f_db, db_info* db_i );
-static int ReadDbPage( sqlite3_file* f_db, db_info* db_i );
-static int GetDbOpenState( Btree* btree, db_info* db_i );
-static int SetDbReservedState( sqlite3* db, Btree* btree, check_crc* crc_impl );
-
-int ReadDbFile( sqlite3_file* f_db, db_info* db_i )
-{
-     int res = SQLITE_ERROR;
-
-     memset( db_i, 0, sizeof( db_info ) );
-
-     res = ReadDbHeader( f_db, db_i );
-     if ( res != SQLITE_OK )
-     {
-          return res;
-     }
-
-     res = ReadDbPage( f_db, db_i );
-
-     return res;
-}
-
-int GetDbOpenState( Btree* btree, db_info* db_i )
-{
-     sqlite3_file* f_db = sqlite3BtreePager( btree )->fd;
-
-     int rc = SQLITE_ERROR;
-     db_i->open_state = DB_CLOSED;
-     
-     if( !isOpen( f_db ) )
-     {
-          rc = sqlite3OsFileSize( f_db, &db_i->file_size );
-          if( rc == SQLITE_IOERR_SHORT_READ )
-          {
-               rc = SQLITE_OK;
-          }
-          
-          if ( rc == SQLITE_OK )
-          {
-               db_i->open_state = db_i->file_size ? 
-                    DB_OPENED_EXISTING : DB_OPENED_CREATING;
-          }
-     }
-
-     return rc;
-}
-
-int SetDbReservedState( sqlite3* db, Btree* btree, check_crc* crc_impl )
-{
-     int rc = SQLITE_ERROR;
-     int reserve = sqlite3BtreeGetReserve( btree );
-     FILE* f_db = 0;
-
-     if ( reserve != crc_impl->xGetCrcLength() && reserve >= 0 )
-     {
-          reserve = crc_impl->xGetCrcLength();
-          rc = sqlite3BtreeSetPageSize( btree, -1, reserve, 0 );
-
-          if ( rc != SQLITE_OK )
-          {
-               sqlite3Error(db, rc, "unable to reserve page space for the codec");
-          }
-     }
-}
-
-int ReadDbHeader( sqlite3_file* f_db, db_info* db_i )
-{
-     char zDbHeader[256];
-     int freelist_trunk_page_number = 0;
-     int rc = SQLITE_ERROR;
-
-     memset( zDbHeader, 0, sizeof( zDbHeader ) );
-          
-     rc = sqlite3OsRead( f_db, zDbHeader, 100, 0 );
-     if( rc == SQLITE_IOERR_SHORT_READ )
-     {
-          rc = SQLITE_OK;
-     }
-
-     if ( rc == SQLITE_OK )
-     {
-          db_i->page_size = ConvertFromBigEndian( zDbHeader + 16, 2 );
-          db_i->page_buff = ( unsigned char* )malloc( db_i->page_size );
-
-          freelist_trunk_page_number = ConvertFromBigEndian( zDbHeader + 32, 4 );
-          db_i->current_page_number = 1;
-          db_i->freelist_trunk_page_number = 
-               freelist_trunk_page_number ? freelist_trunk_page_number : -1;
-          db_i->freelist_trunk_page_offset = 
-               freelist_trunk_page_number ? ( freelist_trunk_page_number - 1 ) * db_i->page_size : -1;
-          db_i->total_number_of_freelist_pages = ConvertFromBigEndian( zDbHeader + 36, 4 );
-          db_i->page_header_offset = 100;
-     }
-
-     return rc;
-}
-
-int ReadDbPage( sqlite3_file* f_db, db_info* db_i )
-{
-     unsigned char page_type = 0;
-     unsigned char reversed[ HASH_SIZE ];
-     int page_length = db_i->page_size;
-     int page_data_lenght = page_length ? page_length - HASH_SIZE : 0; // FIXME сделать обработку первичного старта
-     int md5_check_result = CHECKSUM_ERROR;
-     int rc = SQLITE_ERROR;
-
-     rc = sqlite3OsRead( f_db, db_i->page_buff, page_length, db_i->current_page_offset );
-
-     if ( rc == SQLITE_OK )
-     {
-          memcpy( &page_type, db_i->page_buff, 1 );
-
-          db_i->page_header_offset = 0;
-
-          if ( db_i->current_page_offset == db_i->freelist_trunk_page_offset )
-          {
-               int idx = 0;
-               char* buff_freelist_leaf_page_numbers = 0;
-
-               // количество номеров листовых неиспользуемых страниц
-               int freelist_leaf_page_count = ConvertFromBigEndian( db_i->page_buff + 4, 4 );
-
-               // следующая страница в связанном списке freelist
-               i64 next_freelist_trunk_page = ConvertFromBigEndian( db_i->page_buff, 4 );
-               db_i->freelist_trunk_page_number = 
-                    next_freelist_trunk_page ? next_freelist_trunk_page : -1;
-               db_i->freelist_trunk_page_offset = 
-                    next_freelist_trunk_page ? ( next_freelist_trunk_page - 1 ) * db_i->page_size : -1;
-               
-               // чтение номеров листовых неиспользуемых страниц
-               buff_freelist_leaf_page_numbers = db_i->page_buff + 8;
-               for ( idx = 0; idx < freelist_leaf_page_count; idx++ )
-               {
-                    int page_number = ConvertFromBigEndian( buff_freelist_leaf_page_numbers, 4 );
-                    InserFreePageNumber( page_number );
-                    buff_freelist_leaf_page_numbers += 4;
-               }
-          }
-
-          if ( IsFreePageExist( db_i->current_page_number ) )
-          {
-               RemoveFreePageNamber( db_i->current_page_number );
-               
-               md5_check_result = CHECKSUM_SUCCESS;
-          }
-          else
-          {
-               memcpy( reversed, db_i->page_buff + page_length - HASH_SIZE, HASH_SIZE );
-
-               md5_check_result = CheckMD5( db_i->page_buff, page_data_lenght, reversed );
-          }
-
-          if ( md5_check_result == CHECKSUM_SUCCESS )
-          {
-               db_i->current_page_number++;
-               db_i->current_page_offset += page_length;
-               
-               if ( db_i->current_page_offset < db_i->file_size )
-               {
-                    return ReadDbPage( f_db, db_i );
-               }
-
-               return SQLITE_OK;
-          }
-
-          ClearFreePageNumbers();
-          return SQLITE_ERROR;
-     }
-
-     return rc;
 }
 
 int sqlite3CodecAttach(sqlite3 *db, int nDb, const void *zKey, int nKey)
